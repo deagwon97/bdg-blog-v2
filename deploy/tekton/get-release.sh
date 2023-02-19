68704aa0b2e457f1b62df188b00ae8c78ccbe570
deagwon97 <curiosity806@naver.com>
2023-02-19 17:54:20 +0900

    deploy tekton

---
+++ deploy/tekton/get-release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml > tekton-pipelines-release.yaml
+
+curl https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml > tekton-dashboard-release.yaml
