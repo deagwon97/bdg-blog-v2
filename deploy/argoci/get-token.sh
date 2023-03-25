370fb26d70005fb72f20a6dd99e187ccfae4881d
deagwon97 <curiosity806@naver.com>
2023-03-25 15:37:44 +0900

    update

--- deploy/argoci/get-token.sh
+++ deploy/argoci/get-token.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ARGO_TOKEN="Bearer $(kubectl get secret argo-executor.service-account-token -n argo-ci -o=jsonpath='{.data.token}' | base64 --decode)"
+namespace=bdg-blog-argoci
+
+ARGO_TOKEN="Bearer $(kubectl get secret argo-executor.service-account-token -n $namespace -o=jsonpath='{.data.token}' | base64 --decode)"
 echo $ARGO_TOKEN
