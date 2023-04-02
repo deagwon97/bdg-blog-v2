6f04bbad347dcbd0cf1acc8a3facd2e00bae6b85
deagwon97 <curiosity806@naver.com>
2023-04-02 13:33:49 +0900

    update

--- deploy/argocd/get-key.sh
+++ deploy/argocd/get-key.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+namespace=bdg-blog-argocd
+kubectl -n $namespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
