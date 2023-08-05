2a2069d10de44a36a5010e8bd536dc937d6b7896
deagwon97 <curiosity806@naver.com>
2023-08-05 12:55:37 +0000

    bug fix dose not working telefunc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from 'next/app'
 import { config } from 'telefunc/client'
 import { AnimatePresence } from 'framer-motion'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import * as service from 'server/service/index.telefunc'
 
 const isBrowser = typeof window !== 'undefined'
 if (isBrowser) {
   config.telefuncUrl = '/api/_telefunc'
+  service.onConnect()
 }
 
 import { createTheme, ThemeProvider } from '@mui/material/styles'
@@ -88,7 +91,6 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
         />
       </Head>
-
       <AnimatePresence mode="wait" initial={true}>
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
